efeddb9c5750ff4f0d6acfc8d0fbe51bf100cf29
Luigi <luigi.drnasin12@gmail.com>
2022-04-19 14:54:10 +0200

    Calculator

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ const operators = document.querySelectorAll("[data-operator]");
 let equal = document.querySelector("[data-equal]");
 const clear = document.querySelector("[data-clear]");
 const undo = document.querySelector("[data-undo]");
-const comma = document.querySelector("[data-comma]");
+const dot = document.querySelector("[data-dot]");
 let firstNum = 0;
 let secondNum = 0;
 let operator = "";
 let equalSign = "";
+let dotClicked = 0;
 
 clear.addEventListener("click", () =>{
     clearAll();
@@ -19,6 +20,13 @@ undo.addEventListener("click", () => {
     currentText.innerText = currentText.innerText.slice(0, -1);
     historyText.innerText = historyText.innerText.slice(0, -1);
 })
+
+dot.addEventListener("click", (e) => {
+    if (currentText.innerHTML != "" && dotClicked == 0){
+        currentText.innerHTML += e.target.textContent;
+        dotClicked += 1;
+    };
+});
 
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
@@ -37,6 +45,7 @@ operators.forEach(operat => {
             currentText.innerHTML += operator;
             historyText.innerHTML = currentText.innerHTML;
             currentText.innerHTML = "";
+            dotClicked = 0;
         };
     });
 });
@@ -45,9 +54,33 @@ equal.addEventListener("click", () =>  {
     if (equalSign != "="){
         equalSign = equal.textContent;
         secondNum = currentText.innerHTML;
-        historyText.innerHTML = "";
-        secondNum = Number(secondNum);
-        currentText.innerHTML = operate(operator, firstNum, secondNum);
+        if(secondNum == 0){
+            alert("zero divison!!!")
+            clearAll();
+        }else{
+            historyText.innerHTML = "";
+            secondNum = Number(secondNum);
+            currentText.innerHTML = operate(operator, firstNum, secondNum);
+            clearAllExcept();
+            firstNum = currentText.innerHTML;
+
+            operators.forEach(operat => {
+                operat.addEventListener("click",(e) => {
+                    if (operator != ""){
+                        console.log("You already enter the operator");
+                    }else {
+                        firstNum = currentText.innerHTML;
+                        firstNum = Number(firstNum);
+                        operator = e.target.textContent;
+                        currentText.innerHTML += operator;
+                        historyText.innerHTML = currentText.innerHTML;
+                        currentText.innerHTML = "";
+                        dotClicked = 0;
+                        equal = "";
+                    };
+                });
+            });
+        };
     }else{
         console.log("You can not enter equal more than once.")
     };
@@ -88,10 +121,20 @@ function clearAll(){
     secondNum = 0;
     equalSign = " ";
     operator = "";
+    dotClicked = 0;
 }
 
 function numberSize(){
     return
+}
+
+function clearAllExcept(){
+    historyText.innerText = "";
+    firstNum = 0;
+    secondNum = 0;
+    equalSign = " ";
+    operator = "";
+    dotClicked = 0;
 }
 
 
